90c0a912032a826f88204aaded44724788fd627f
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-08-03 18:54:27 +0200

    create Radi button interface

---
+++ components/Radio/index.tsx
@@ -0,0 +1,28 @@
+import { Pressable, Text, View } from "react-native";
+
+interface RadioInterface {
+  checked: boolean;
+  onPress: () => void;
+  style?: {}
+}
+
+export default function Radio(props: RadioInterface) {
+  console.log(props.checked);
+  
+  return (
+    <Pressable onPress={props.onPress} style={props.style}>
+      <View style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 30,
+        height: 30,
+        borderColor: props.checked ? 'green' : 'white',
+        borderWidth: 2,
+        borderRadius: 100,
+        backgroundColor: props.checked ? 'green' : 'transparent',
+      }}>
+        {props.checked ? <Text style={{color: 'white'}}>✓</Text> : null}
+      </View>
+    </Pressable>
+  )
+}
