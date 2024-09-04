0efc9eece88e31aa426403354b2a9497afaccc91
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-04 23:00:56 +0200

    change color text

--- components/StyledText/index.tsx
+++ components/StyledText/index.tsx
@@ -101,7 +101,7 @@ export default function StyledText(props: Readonly<TextProps>) {
   const getColorStyles = () => {
     return [
       props.light && {
-        color: mode === 'light' ? Colors.light["palette-1"] : Colors.dark["palette-1"],
+        color: mode === 'light' ? Colors.light["palette-11"] : Colors.dark["palette-11"],
       },
       props.transparent && { color: 'transparent' },
       props.button && [
