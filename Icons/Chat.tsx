3868cf32ed0ad3a81acba858216f650b83f11012
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-12 17:39:31 +0200

    add dark mode Chat Icon

--- Icons/Chat.tsx
+++ Icons/Chat.tsx
@@ -1,6 +1,26 @@
 import { Path, Rect, Svg } from "react-native-svg";
+import useScreenMode from "../utilities/screenMode";
+import { Colors } from "../constants/Colors";
 
-export default function Chat () {
+interface ChatProps {
+  readonly black?: boolean;
+}
+
+export default function Chat(props: ChatProps) {
+
+  const { mode } = useScreenMode()
+
+  const calcFill = () => {
+    // props.black ? mode==='light'? Colors.light["palette-11"] : Colors.dark["palette-11"] : 'rgba(255, 255, 255, 0)'
+    if (props.black && mode === 'light') {
+      return Colors.light["palette-11"];
+    } else if (props.black && mode === 'dark') {
+      return Colors.dark["palette-11"];
+    } else {
+      return 'rgba(255, 255, 255, 0)';
+    }
+  }
+
   return (
     <Svg
     width={24}
@@ -16,12 +36,12 @@ export default function Chat () {
       width={16}
       height={12}
       rx={2}
-      stroke="#33363F"
+      stroke={calcFill()}
       strokeWidth={2}
     />
     <Path
       d="M4 9L11.1056 12.5528C11.6686 12.8343 12.3314 12.8343 12.8944 12.5528L20 9"
-      stroke="#33363F"
+      stroke={calcFill()}
       strokeWidth={2}
     />
   </Svg>
