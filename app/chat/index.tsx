0a167ec2fe77e37e152a611e89f343ec52f29aac
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-12 18:18:07 +0200

    mejorar UX y UY pagina chat

--- app/chat/index.tsx
+++ app/chat/index.tsx
@@ -1,5 +1,5 @@
-import { Link, Stack } from "expo-router";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { Link, router, Stack } from "expo-router";
+import { ImageBackground, ScrollView, StyleSheet, View, TouchableOpacity } from "react-native";
 import { Colors } from "../../constants/Colors";
 import StyledText from "../../components/StyledText";
 import Like from "../../Icons/Like";
@@ -11,8 +11,13 @@ import { useEffect, useState } from "react";
 import getAllChats from "../../api/chat/getAllChats";
 import CitasCiegas from "../../Icons/CitasCiegas";
 import useScreenMode from "../../utilities/screenMode";
+import menuEnabled from "../../api/menu/menuenabled";
+import getAllMatches from "../../api/user/getAllMatches";
+import useAuth from "../../utilities/login";
 
 export default function ChatPage() {
+
+  const { getToken } = useAuth()
 
   useEffect(() => {
     const fetchChats = async () => {
@@ -20,13 +25,135 @@ export default function ChatPage() {
       setChats(data)
     }
     fetchChats()
-    
   }, [])
 
   const [chats, setChats] = useState<{
     ID: number,
     name: string,
   }[]>([])
+
+  const [menuOptions, setMenuOptions] = useState([
+    {
+      id: 0,
+      text: "",
+      selected: false,
+      url: "",
+      icon: <></>,
+      active: false,
+    },
+  ]);
+
+  const [matchList, setMatchList] = useState([
+    {
+      "id": 12,
+      "email": "rbs3@g.com",
+      "phone": "666666666",
+      "passwd": "$2a$10$NMYr7J3kpZpIvGvCQ3R6.OCGCXGPkhN7KPgHhcv9G.Anhru5MpGLS",
+      "isActive": 1,
+      "id_genre": 1,
+      "name": "Roger 1",
+      "birthdate": "1992-05-30T22:00:00.000Z",
+      "id_find": null,
+      "id_orientation": null,
+      "id_status": null,
+      "bio": null,
+      "height": null,
+      "studyPlace": null,
+      "you_work": null,
+      "charge_work": null,
+      "enterprise": null,
+      "drink": null,
+      "educative_level_id": null,
+      "personality": null,
+      "id_zodiac": null,
+      "mascotas": null,
+      "id_religion": null,
+      "genre_name": "Hombre",
+      "roleName": null,
+      "findText": null,
+      "orientationText": null,
+      "statusText": null,
+      "fotos": [
+        "https://i.pinimg.com/280x280_RS/74/43/c2/7443c297a5735055c4485538f5596086.jpg",
+        "https://i.pinimg.com/280x280_RS/74/43/c2/7443c297a5735055c4485538f5596086.jpg"
+      ],
+      "link": "12",
+    }
+  ])
+
+  useEffect(() => {
+    const getMenu = async () => {
+      const matches = await menuEnabled({ key: 'matches' });
+      const events = await menuEnabled({ key: 'events' });
+      const calendar = await menuEnabled({ key: 'calendar' });
+      const chat = await menuEnabled({ key: 'chat' });
+      const citasCiegas = await menuEnabled({ key: 'ciegas' });
+      const aux = []
+      if (matches.Valor === '1') {
+        aux.push({
+          id: 1,
+          text: "MATCHES",
+          selected: false,
+          url: "/matches",
+          icon: <Like black />,
+          active: false,
+        });
+      }
+      if (events.Valor === '1') {
+        aux.push(
+          {
+            id: 2,
+            text: "EVENTS",
+            selected: false,
+            url: "/events",
+            icon: <Party />,
+            active: false,
+          }
+        );
+      }
+      if (calendar.Valor === '1') {
+        aux.push({
+          id: 3,
+          text: "CALENDAR",
+          selected: false,
+          url: "/calendar",
+          icon: <CalendarIcon />,
+          active: false,
+        });
+      }
+      if (chat.Valor === '1') {
+        aux.push({
+          id: 4,
+          text: 'CHAT',
+          selected: true,
+          url: '/chat',
+          icon: <Chat black />,
+          active: true,
+        });
+      }
+      if (citasCiegas.Valor === '1') {
+        aux.push({
+          id: 5,
+          text: 'CITAS A CIEGAS',
+          selected: false,
+          url: '/citasCiegas',
+          icon: <CitasCiegas />,
+          active: false,
+        });
+      }
+      setMenuOptions(aux);
+    };
+    getMenu();
+  }, []);
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const token = await getToken() ?? ''
+      const data = await getAllMatches({ token })
+      setMatchList(data.matchList)
+    }
+    fetchMatches()
+  }, [])
 
   const { mode } = useScreenMode()
 
@@ -37,9 +164,67 @@ export default function ChatPage() {
           headerTitle: () => null,
         }}
       />
-      <View style={[styles.container, {
-        backgroundColor: (mode === 'light') ? Colors.light["palette-3"] : Colors.dark["palette-3"],
-      }]}>
+
+      <View style={
+        [
+          styles.container,
+          { backgroundColor: (mode === 'light') ? Colors.light["palette-3"] : Colors.dark["palette-3"], }
+        ]
+      }>
+        <View style={[
+          styles.box,
+          styles.box1,
+          styles.menu,
+          {
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            backgroundColor: 'red',
+            marginBottom: 10,
+            flex: 1.5
+          }
+        ]}>
+          <ScrollView horizontal={true}>
+            {
+              matchList.map((match, index) => (
+                <View key={index + 1} style={{ alignItems: 'center', marginHorizontal: 5 }}>
+                  <TouchableOpacity onPress={() => {
+                    router.push(`/chat/${match.link}`)
+                  }}>
+                    <View
+                      style={{
+                        borderRadius: 25, // half of the width and height to make it fully circular
+                        overflow: 'hidden', // this ensures the image stays within the circle
+                        backgroundColor: mode === 'light' ? Colors.light['palette-5'] : Colors.dark['palette-5'],
+                        marginVertical: 'auto',
+                        marginHorizontal: 10,
+                        height: 50,
+                        width: 50,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginBottom: 5,
+                      }}
+                    >
+                      <ImageBackground
+                        source={{ uri: match.fotos[0] }}
+                        style={{
+                          width: 50,
+                          height: 50,
+                        }}
+                      />
+                    </View>
+                    <View>
+                      <StyledText xsmall bold>
+                        {match.name}
+                      </StyledText>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              ))
+            }
+          </ScrollView>
+        </View>
         <View style={[styles.box, styles.box2, {
           backgroundColor: (mode === 'light') ? Colors.light["palette-3"] : Colors.dark["palette-3"],
         }]}>
@@ -67,50 +252,7 @@ export default function ChatPage() {
             </ScrollView>
           </View>
         </View >
-        <Menu
-          options={[
-            {
-              id: 1,
-              text: "MATCHES",
-              selected: true,
-              url: "/matches",
-              icon: <Like black />,
-              active: false,
-            },
-            {
-              id: 2,
-              text: "EVENTS",
-              selected: false,
-              url: "/events",
-              icon: <Party />,
-              active: false,
-            },
-            {
-              id: 3,
-              text: "CALENDAR",
-              selected: false,
-              url: "/calendar",
-              icon: <CalendarIcon />,
-              active: false,
-            },
-            {
-              id: 4,
-              text: 'CHAT',
-              selected: false,
-              url: '/chat',
-              icon: <Chat />,
-              active: true,
-            },
-            {
-              id: 5,
-              text: 'CITAS A CIEGAS',
-              selected: false,
-              url: '/citasCiegas',
-              icon: <CitasCiegas />,
-              active: false,
-            }
-          ]}
-        />
+        <Menu options={menuOptions} />
       </View >
     </>
   )
@@ -165,5 +307,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     textAlign: 'center',
     marginTop: 10,
+  },
+  menu: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+    // borderBottomWidth: 1,
+    width: '100%',
   },
 });
