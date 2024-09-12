9bb2337f947e5da985190ad516d6c0fb4e47e991
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-12 18:27:18 +0200

    general fix

---
+++ api/chat/getChatData.ts
@@ -0,0 +1,24 @@
+const getChatData = async (id: number, token: string) => {
+  try {
+    let API_URL = '';
+    
+    API_URL = 'https://api.bledbonds.es/api/v1';
+    const resp = await fetch(`${API_URL}/chat/${id}`, {
+      method: 'GET',
+      headers: {
+        'x-api-key': '6d83d4496c0010950eb2f3a0db79004c',
+        'user-token': token,
+      }
+    });
+    if (!resp.ok) {
+      throw new Error('Network response was not ok');
+    } else {
+      const data = await resp.json();
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default getChatData;
--- api/user/getAllMatches.ts
+++ api/user/getAllMatches.ts
@@ -2,11 +2,11 @@ interface RegisterInterface {
   token: string;
 }
 
-const getToLike = async (user: RegisterInterface) => {
+export default async function getAllMatches(user: RegisterInterface) {
   try {
     let API_URL = '';
     API_URL = 'https://api.bledbonds.es/api/v1';
-    const resp = await fetch(`${API_URL}/users/get/toLike`, {
+    const resp = await fetch(`${API_URL}/users/get/matchList`, {
       method: 'GET',
       headers: {
         'x-api-key': '6d83d4496c0010950eb2f3a0db79004c',
@@ -14,15 +14,7 @@ const getToLike = async (user: RegisterInterface) => {
       },
     });
     if (!resp.ok) {
-      if (resp.status === 404) {
-        const data = await resp.json();
-        return {
-          status: '-0001',
-          data
-        };
-      } else {
-        throw new Error('Network response was not ok');
-      }
+      throw new Error('Network response was not ok');
     } else {
       const data = await resp.json();
       return data;
@@ -31,5 +23,3 @@ const getToLike = async (user: RegisterInterface) => {
     console.error(error);
   }
 }
-
-export default getToLike;
