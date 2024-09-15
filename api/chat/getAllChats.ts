900a98e50a622c3779933ef5967da0fe8dbb90a3
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-15 20:39:01 +0200

    add token in get Chats

--- api/chat/getAllChats.ts
+++ api/chat/getAllChats.ts
@@ -1,11 +1,13 @@
-const getAllChats = async () => {
+const getAllChats = async (token: string) => {
   try {
     let API_URL = 'https://api.bledbonds.es/api/v1';
+    console.log(token);
     
     const resp = await fetch(`${API_URL}/chat/`, {
       method: 'GET',
       headers: {
-        'x-api-key': '6d83d4496c0010950eb2f3a0db79004c'
+        'x-api-key': '6d83d4496c0010950eb2f3a0db79004c',
+        'user-token': token,
       }
     });
     if (!resp.ok) {
