4dccc022a1422e275b7420b7756c7abb830f3e80
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-12-11 17:33:30 +0100

    add mode

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import useAuth from './utilities/login';
 import { enableScreens } from 'react-native-screens';
+import useScreenMode from './utilities/screenMode';
+import { Colors } from './constants/Colors';
 
 export default function App() {
   enableScreens();
   const { isLoggedIn } = useAuth();
-
+  const { mode } = useScreenMode()
   return (
     isLoggedIn ? (
-      <></>
+      <View style={[styles.container, {
+        backgroundColor: (mode === 'dark') ? Colors.dark['palette-3'] : Colors.light['palette-3'],
+      }]}>
+      </View>
     ) : (
-      <View style={styles.container}>
+      <View style={[styles.container, {
+        backgroundColor: (mode === 'dark') ? Colors.dark['palette-3'] : Colors.light['palette-3'],
+      }]}>
         <StatusBar style="auto" />
         <Text>Open up App.js to start working on your app!</Text>
-        <Link href="/about">Go to About</Link>
       </View>
     )
   );
@@ -24,7 +29,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
